736b2887bbb56d869562f286df36809a5655b66a
rohan <rohanrv2004@gmail.com>
2026-05-07 00:29:08 +0530

    fic: vercel

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/auth", "@repo/ui", "db"],
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: 'https://scheduler-9smh.onrender.com/api/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
